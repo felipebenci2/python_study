61c7e9ca473e587aca0c7a2dbf770913018e48b1
felipebenci2 <felipe.benci2@icloud.com>
2025-12-09 10:54:39 -0300

    Update and rename Exercício_1 to Exercício_1.py

---
+++ Modulo_1/Exercícios/Exercício_1.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+cidade = input('Onde você mora? ')
+
+print(f'Seu nome é {nome}, você tem {idade} anos e mora em {cidade}.')
