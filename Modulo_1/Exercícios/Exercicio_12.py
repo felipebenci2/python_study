79893aba1bdacc1d3bbacd0cef1c058d44a04be6
felipebenci2 <felipe.benci2@icloud.com>
2025-12-10 12:50:52 -0300

    Create Exercicio_12.py

---
+++ Modulo_1/Exercícios/Exercicio_12.py
@@ -0,0 +1,10 @@
+nome = input('Digite um nome: ')
+idade = int(input('Digite uma idade: '))
+altura = int(input('Digite a altura em cm: ')) / 100
+peso = float(input('Digite o peso: '))
+print(f''' 
+nome = {nome}
+idade = {idade} anos
+altura = {altura}m
+peso = {peso}kg
+''')
