161e772e99ac3b95888e34f81d7e12346df30672
felipebenci2 <felipe.benci2@icloud.com>
2025-12-10 13:46:46 -0300

    Create Exercicio_20.py

---
+++ Modulo_1/Exercícios/Exercicio_20.py
@@ -0,0 +1,13 @@
+n1 = int(input('Insira um valor: '))
+n2 = int(input('Insira outro valor: '))
+soma = n1 + n2
+diferenca = abs(n1 - n2)
+produto = n1 * n2
+divisao = n1 / n2
+
+print(f'''
+soma = {soma}
+diferenca = {diferenca}
+produto = {produto}
+divisao = {divisao}
+''')
