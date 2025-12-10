ec35fb7ecf071cfc8092e9f7d34a5f9f991a0b77
felipebenci2 <felipe.benci2@icloud.com>
2025-12-10 11:35:46 -0300

    Update Exercicio_4.py

--- Modulo_1/Exercícios/Exercicio_4.py
+++ Modulo_1/Exercícios/Exercicio_4.py
@@ -1,1 +1,6 @@
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+total = n1 - n2
+
+print(f'A subtração dos valores {n1} e {n2} resulta no produto de {total}.')
 
