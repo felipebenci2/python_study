6e2ef294dd60f757fdab1e8baf97a03504d65ed5
felipebenci2 <felipe.benci2@icloud.com>
2025-12-10 11:34:59 -0300

    Update Exercicio_3.py

--- Modulo_1/Exercícios/Exercicio_3.py
+++ Modulo_1/Exercícios/Exercicio_3.py
@@ -2,5 +2,5 @@ n1 = int(input('Digite um número inteiro: '))
 n2 = int(input('Digite outro número inteiro: '))
 total = n1 + n2
 
-print(f'A soma dos valores {n1} e {n2} restulta no produto de {total}.')
+print(f'A soma dos valores {n1} e {n2} resulta no produto de {total}.')
 
