e2c88fd54a23e00bc7b4c730f5c9f0f3f325c19b
felipebenci2 <felipe.benci2@icloud.com>
2025-12-11 08:07:54 -0300

    Update Desafio_3.py

--- Modulo_1/Desafios/Desafio_3.py
+++ Modulo_1/Desafios/Desafio_3.py
@@ -1,1 +1,16 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite quantos anos você tem: '))
+altura = int(input('Digite sua altura em centímetros. Ex: 169: ')) / 100
+peso = float(input('Digite seu peso: '))
 
+resultado = peso / (altura * altura)
+
+print(f'''
+======= RELATÓRIO DE PERFIL =======
+Nome: {nome}
+Idade: {idade} anos
+Altura: {altura:.2f} m
+Peso: {peso:.2f} kg
+IMC: {resultado:.2f}
+===================================
+ ''')
