8f02e77ffb79501dd080aadf8bd7e8f9660621a5
felipebenci2 <felipe.benci2@icloud.com>
2025-12-11 08:03:37 -0300

    Update Desafio_1.py

--- Modulo_1/Desafios/Desafio_1.py
+++ Modulo_1/Desafios/Desafio_1.py
@@ -1,3 +1,5 @@
+import math 
+
 n1 = int(input('Insira um valor: ')) 
 n2 = int(input('Insira um outro valor: '))
 soma = n1 + n2
@@ -6,11 +8,16 @@ multi = n1 * n2
 div = n1 / n2
 div_exata = n1 // n2
 potencia = n1 ** n2
+raiz1 = math.sqrt(n1)
+raiz2 = math.sqrt(n2)
 
 print(f'''A seguir o produto das funções dos números {n1} e {n2}:
 soma = {soma}
 subtração = {sub}
 multiplicação = {multi}
-divisão = {div}
-divisão exata = {div_exata}
-potência = {potencia}''')
+divisão = {div:.3f}
+divisão exata = {div_exata:.3f}
+potência = {potencia}
+raiz quadrada 1 = {raiz1:.3f}
+raiz quadrada 2 = {raiz2:.3f}
+''')
