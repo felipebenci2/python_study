2cbdf76ea17a4e809b4eec0510417547b5af3d14
felipebenci2 <felipe.benci2@icloud.com>
2025-12-11 08:08:15 -0300

    Update Desafio_2.py

--- Modulo_1/Desafios/Desafio_2.py
+++ Modulo_1/Desafios/Desafio_2.py
@@ -1,16 +1,11 @@
-nome = input('Digite seu nome: ')
-idade = int(input('Digite quantos anos você tem: '))
-altura = int(input('Digite sua altura em centímetros. Ex: 169: ')) / 100
-peso = float(input('Digite seu peso: '))
+valor_real = float(input('Insira o valor em REAIS(R$) para fazer a conversão: '))
+valor_euro = valor_real / 6.42
+valor_libra = valor_real / 7.34
+valor_iene = valor_real / 0.035
 
-resultado = peso / (altura * altura)
-
-print(f'''
---- Dados do Uuário ---
-nome = {nome}
-idade = {idade}
-altura = {altura}
-peso = {peso}
-
-Resultado IMC: {resultado:.2f}
- ''')
+print(f''' 
+O valor de R${valor_real:.2f} convertido para:
+Euro: €{valor_euro:.2f}
+Libra Esterlina: £{valor_libra:.2f}
+Iene: ¥{valor_iene:.2f}
+''')
