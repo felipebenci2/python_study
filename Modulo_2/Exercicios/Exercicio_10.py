15e2102d03ba9dc4cc781486bed26beafa29a685
felipebenci2 <felipe.benci2@icloud.com>
2025-12-12 13:49:56 -0300

    Add Exercicio_10.py to check number in range

---
+++ Modulo_2/Exercicios/Exercicio_10.py
@@ -0,0 +1,6 @@
+n = int(input('Insira um número: '))
+intervalo = 5, 6, 7, 8 ,9 ,10, 11, 12, 13, 14, 15
+if n in intervalo:
+    print(f'{n} está dentro do intervalo.')
+else:
+    print('Não está no intervalo')
