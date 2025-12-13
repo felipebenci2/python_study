de5e2a30d2c0d89f1fb657e3203cb1df1d58265b
felipebenci2 <felipe.benci2@icloud.com>
2025-12-13 08:54:27 -0300

    Add user authentication script in Exercicio_13.py

---
+++ Modulo_2/Exercicios/Exercicio_13.py
@@ -0,0 +1,7 @@
+user = input('Insira o usuário: ')
+senha = input('Insira a senha: ')
+if user == 'admin' and senha == '1234':
+    print('Login realizado')
+else:
+    print('Usuário ou senha inválidos')
+    
