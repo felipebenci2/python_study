e0ca8f679af6763e1e593442d8d06e8cfb84c78f
felipebenci2 <felipe.benci2@icloud.com>
2025-12-13 17:57:47 -0300

    Add student grade evaluation script

---
+++ Modulo_2/Desafios/Desafio_3.py
@@ -0,0 +1,18 @@
+nome = input('Nome do aluno: ')
+nota = float(input('Insira a nota: '))
+if nota > 10 or nota < 0:
+    status = 'Nota inválida'
+elif nota > 9:
+    status = 'Excelente'
+elif nota >= 7:
+    status = 'Bom'
+elif nota >= 5:
+    status = 'Regular'
+elif nota < 5:
+    status = 'Reprovado'
+print(f'''
+===== Dados do Aluno =====
+Nome: {nome}
+Nota: {nota}
+Status: {status}
+''')
