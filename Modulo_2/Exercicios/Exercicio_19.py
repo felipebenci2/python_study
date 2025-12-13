c6d5e91e226a64b0dde7f7dccbc741ad54c4fe13
felipebenci2 <felipe.benci2@icloud.com>
2025-12-13 13:13:54 -0300

    Add BMI calculator in Exercicio_19.py

---
+++ Modulo_2/Exercicios/Exercicio_19.py
@@ -0,0 +1,24 @@
+nome = input('Digite seu nome: ')
+idade = int(input('Digite quantos anos você tem: '))
+altura = int(input('Digite sua altura em centímetros. Ex: 169: ')) / 100
+peso = float(input('Digite seu peso: '))
+
+resultado = peso / (altura * altura)
+
+print(f'''
+--- Dados do Uuário ---
+nome = {nome}
+idade = {idade}
+altura = {altura}
+peso = {peso}
+
+Resultado IMC: {resultado:.2f}
+ ''')
+if resultado < 18.5:
+    print('Abaixo do peso')
+elif resultado >= 30:
+    print('Obesidade')
+elif resultado >= 25 :
+    print('Sobrepeso')
+elif resultado >= 18.5:
+    print('Peso normal')
