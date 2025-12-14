85296203926c91649493a48af5a309ebca49caf1
felipebenci2 <felipe.benci2@icloud.com>
2025-12-14 20:24:58 -0300

    Fix input conditions for menu options

--- Modulo_3/Exercicios/Exercicio_09.py
+++ Modulo_3/Exercicios/Exercicio_09.py
@@ -8,14 +8,14 @@ while True:
     ''')
         opcao = input('Insira uma opçao: ')
 
-        if opcao == '1' or 'olá' in opcao.lower():
+        if opcao == '1' or opcao == 'ola':
                 print('Olá')
 
-        elif opcao == '2' or 'data' in opcao.lower():
+        elif opcao == '2' or opcao == 'Data' or opcao == 'Atual':
                 hoje = date.today()
                 print(hoje)
-                
-        elif opcao == '3' or 'sair' in opcao.lower():
+
+        elif opcao == '3' or opcao == 'Data':
                 print('Fim')
                 break
         else:
